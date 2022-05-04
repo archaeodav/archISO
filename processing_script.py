050510b8ed5a123288d89db41ec9ff23948dd44d
David Stott <67315670+archaeodav@users.noreply.github.com>
2022-05-04 14:57:07 +0200

    Update processing_script.py

--- processing_script.py
+++ processing_script.py
@@ -9,25 +9,130 @@ import os
 
 from qgis import processing
 
+from qgis.core import (
+  QgsApplication,
+  QgsDataSourceUri,
+  QgsCategorizedSymbolRenderer,
+  QgsClassificationRange,
+  QgsPointXY,
+  QgsProject,
+  QgsExpression,
+  QgsField,
+  QgsFields,
+  QgsFeature,
+  QgsFeatureRequest,
+  QgsFeatureRenderer,
+  QgsGeometry,
+  QgsGraduatedSymbolRenderer,
+  QgsMarkerSymbol,
+  QgsMessageLog,
+  QgsRectangle,
+  QgsRendererCategory,
+  QgsRendererRange,
+  QgsSymbol,
+  QgsVectorDataProvider,
+  QgsVectorLayer,
+  QgsVectorFileWriter,
+  QgsWkbTypes,
+  QgsSpatialIndex,
+  QgsVectorLayerUtils
+)
 
-from qgis.core import (QgsProcessingAlgorithm,
-       QgsProcessingParameterNumber,
-       QgsProcessingParameterFeatureSource,
-       QgsProcessingParameterFeatureSink)
 
-
-class rWalk(QgsProcessingAlgorithm):
+class InputPoints():
     
-    def __init__(self):
-        super().__init__()
+    def __init__(self,
+                 pts,
+                 temp_dir):
         
-    def name(self):
-        return "rWalk"
-    
-    def displayName(self):
-        return "rWalk Script"
-    
-    def createInstance(self):
-        retrun type(self)()
+        self.pts = pt
+        
+        self.temp_dir = temp_dir
+        
         
     
+    def buff(self,
+             buff_dist):
+        
+        p = processing.run('native:buffer',{'INPUT': self.points,
+                                            'DISTANCE': buff_dist,
+                                            'SEGMENTS':25,
+                                            'DISSOLVE':True,
+                                            'END_CAP_STYLE':0,
+                                            'JOIN_STYLE':0,
+                                            'MITER_LIMIT':10,
+                                            'OUTPUT':os.path.join(self.temp_dir,'buffers.shp')
+                                            })
+        
+        return p['OUTPUT']
+    
+    def centroids(self,
+                  buffers):
+        
+        p = processing.run('native:centroids',{'INPUT':buffers,
+                                               'ALL_PARTS':True,
+                                               'OUTPUT':os.path.join(self.temp_dir,'centroids.shp')
+                                               })
+                                               
+        return p['OUTPUT']
+        
+    
+        
+    def run(self,
+            buff_dist=25000):
+        
+        buffers = self.buff(buff_dist)
+        
+        centroids = self.centroids(buffers)
+        
+        return centroids
+        
+    
+class LCP():
+    
+    def __init__(self,
+                 points,
+                 elevation,
+                 friction,
+                 temp_dir,
+                 output_dir):
+        
+        self.points = QgsVectorLayer(points, "points", "ogr")
+        self.elevation = elevation
+        self.friction = friction
+        self.temp_dir = temp_dir
+        self.output_dir = output_dir
+        
+    def walk(self,
+             point):
+        
+        p = processing.run('grass7:r.walk.coords',{'elevation':self.elevation,
+                                                   'friction':self.friction,
+                                                   'start_coordinates':self.})
+        
+        
+        
+        pass
+    
+    def drain(self):
+        pass
+    
+    def cleanup(self):
+        pass
+    
+    def run(self,
+            cleanup=False):
+        
+        features = self.pts.getFeatures()
+        
+        for f in features:
+            geom = f.geometry()
+            pt = geom.asPoint()
+            
+            
+    
+
+
+    
+
+
